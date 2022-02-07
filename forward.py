8597f81eefebdd848a55f52da24d253436551c85
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2022-02-06 21:05:47 -0300

    Update forward.py

--- forward.py
+++ forward.py
@@ -1,14 +1,23 @@
-from readFile import read
+import var
 
-class forward():
-    def __init__(self):
-        self.__knowledgeBase = read()
-        self.start()
-
-    def start(self):
-        for rule in self.__knowledgeBase.getRules():
-            print(rule.IF, rule.THEN)
+class Forward():
+    def search(self, addDiagnoses):
         
-
-
-forward()
+        if var.vars["dor_de_cabeça"]:
+            addDiagnoses('receitar = analgésico')
+        if var.vars["dor_de_cabeça"] and var.vars["garganta_inflamada"] and var.vars["tosse"]:
+            addDiagnoses('diagnóstico = gripe')
+        if var.vars["cansaço"] and var.vars["dor_de_cabeça"]:
+            addDiagnoses('diagnóstico = mononucleose_infecciosa')
+        if var.vars["cansaço"] and var.vars["garganta_inflamada"]:
+            addDiagnoses('diagnóstico = amigdalite')
+        if var.vars["cansaço"]:
+            addDiagnoses('diagnóstico = estresse')
+        if var.vars["garganta_inflamada"] and var.vars["dor_de_garganta"] and var.vars["dor_de_ouvido"]:
+            addDiagnoses('diagnóstico = faringite_estreptocócica')
+        if var.vars["cansaço"] and var.vars["privação_de_sono"]:
+            addDiagnoses('diagnóstico = apneia_de_sono')
+        if var.vars["dor_de_ouvido"]:
+            addDiagnoses('diagnóstico = otite')
+        if var.vars["garganta_inflamada"]:
+            addDiagnoses('receitar = Anti-inflamatórios')
